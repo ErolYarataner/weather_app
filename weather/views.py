eff145ec7b7b91eb45e6775cdb2cd1164de82c73
ErolYarataner <eyarataner@gmail.com>
2021-01-13 18:34:36 +0100

    api url eklendi

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 def index(request):
+    city = 'Istanbul'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=20c1238224c0c4f50dadbd66f77b4a1a'
+
+    response = requests.get(url.format(city))
+
+    print(response.text)
+
     return render(request, 'weather/index.html')
